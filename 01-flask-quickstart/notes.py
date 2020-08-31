315d48a22c3f2241aff8f437308d5286aa22af8b
Vulpine <>
2020-08-31 09:01:32 +0200

    01-flask-quickstart notes added

---
+++ 01-flask-quickstart/notes.py
@@ -0,0 +1,21 @@
+"""
+flask-quickstart app structure:
+    ROOT
+    -- main.py
+    -- config.py
+    -- WEBSITE
+        -- __init__.py
+        -- routing.py
+        -- forms.py
+        -- models.py
+        -- STATIC
+            -- CSS
+                -- (OPT)bootstrap.min.css
+            -- JS
+        -- TEMPLATES
+            -- PAGES
+                -- page1.html
+                -- page2.html
+                -- ...
+            -- base.html
+"""
