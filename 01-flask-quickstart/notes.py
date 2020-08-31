b794363491a69fa1cdb9f2a13dde1eea4223f560
Vulpine <>
2020-08-31 09:33:14 +0200

    Explanation on file struct

--- 01-flask-quickstart/notes.py
+++ 01-flask-quickstart/notes.py
@@ -1,21 +1,29 @@
 """
 flask-quickstart app structure:
     ROOT
-    -- main.py
-    -- config.py
-    -- WEBSITE
-        -- __init__.py
-        -- routing.py
-        -- forms.py
-        -- models.py
-        -- STATIC
+    -- main.py              <-- the entry point of the app
+    -- config.py            <-- separates config for ease of use
+    -- WEBSITE              <-- actual content goes here
+        -- __init__.py      <-- creates the app and imports modules before we do anything
+        -- routing.py       <-- handles url-function pairing
+        -- forms.py         <-- separates forms for ease of use
+        -- models.py        <-- separates database models for ease of use
+        -- STATIC           <-- files to be served along w/ the website
             -- CSS
-                -- (OPT)bootstrap.min.css
+                -- (OPT)bootstrap.min.css   <-- a CDN is recommended instead
+                -- site.css                 <-- if you have custom css 
             -- JS
-        -- TEMPLATES
-            -- PAGES
+        -- TEMPLATES            <-- JINJA templating here
+            -- PAGES            <-- you'll use these in render_template directly
                 -- page1.html
                 -- page2.html
                 -- ...
-            -- base.html
+            -- base1.html        <-- pages inherit from these
+            -- base2.html        <-- pages inherit from these
+
+The reason for using this will become clear later.
+Much simpler structures are possible (you can create an entire app in a single file),
+but as projects grow maintainence would become cumbersome.
+
+https://en.wikipedia.org/wiki/Separation_of_concerns
 """
